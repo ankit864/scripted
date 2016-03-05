238d63b6018fc64f8e072458ea7ef09d2ccbfe6c
ankit <ankit.1313025@kiet.edu>
2016-03-06 00:34:16 +0530

    added all logs define in rsyslog

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 day=`date +%d`
 month=`date +%m`
 year=`date +%Y`
@@ -15,53 +16,65 @@ if [ ! -d $back_dir ]; then
 	mkdir -p $back_dir
 fi
  
-#for var
-
-
-#: '
 backup()
 {
-#var_dir=${!1}
-#echo $var_dir
  
 if [ ! -d $var_dir ];then
 	mkdir -p $var_dir
 	rsync -r /var/log/$var.*.gz $var_dir 2>> $report_dir/$var.txt && rsync -r /var/log/$var.1 $var_dir  2>> $report_dir/$var.txt
-	#$var_dir 
-#&& rm -rf /var/log/$var.*.gz 
+	 
+ 
 	var1=$?
 	if [ $var1 -eq 0 ];then
-		echo -e "[`date +%d-%m-%Y`]\n" 
-		echo "\nvar backup done on `date`\n" >> $report_dir/$var.txt 
+		echo  "[`date +%d-%m-%Y`]" >> $report_dir/$var.txt
+		echo -e "\n var backup done on `date` \n" >> $report_dir/$var.txt 
 	else
-		echo -e "[`date +%d-%m-%Y`]\n"
+		echo  "[`date +%d-%m-%Y`]" >> $report_dir/$var.txt
 		echo -e "\n backup is not done due to above errors  on `date` \n" >> $report_dir/$var.txt
 	fi
 	else 
-		echo "backup done on same day `date` " >>  $report_dir/$var.txt
+		echo  "[`date +%d-%m-%Y`]" >> $report_dir/$var.txt		
+		echo "Backup is Done on Same Day `date` " >>  $report_dir/$var.txt
 fi
 
 }
 #'
+}
+
 var="auth.log"
 var_dir="$back_dir/$var""_backup"
-echo $var_dir
+clean_log 
+#echo $var_dir
+#: '
 backup $var_dir
 
 #if [ 1 -eq 1 ];then
 var="alternatives.log"
 var_dir="$back_dir/$var""_backup"
-backup $var_dir
+clean_log
+#
+
+#backup $var_dir
 #echo $var_dir
 #fi
-var="alternatives.log"
+var="cron.log"
 var_dir="$back_dir/$var""_backup"
 backup $var_dir
 #
-var="alternatives.log"
+var="kern.log"
 var_dir="$back_dir/$var""_backup"
 backup $var_dir
 #
-var="alternatives.log"
+var="daemon.log"
 var_dir="$back_dir/$var""_backup"
 backup $var_dir
+#
+var="user.log"
+var_dir="$back_dir/$var""_backup"
+backup $var_dir
+#
+var="sys.log"
+var_dir="$back_dir/$var""_backup"
+backup $var_dir
+
+
